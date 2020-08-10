c50714bfd7927c373cd12b2f2efdc2ecc76758f7
Jon Hardy <jonathan.c.hardy@gmail.com>
2020-08-10 15:13:35 -0400

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Repro:
+1) Install docfx via Github zip file: https://github.com/dotnet/docfx/releases/tag/v2.56.2
+2) Add to docfx to PATH
+3) Run `docfx --serve`
+4) Open browser to http://localhost:8080/api/DocFXTest.Test.html. **Quotes inside code blocks are broken.**
---
+++ src/Test.cs
@@ -0,0 +1,16 @@
+namespace DocFXTest {
+  /// <summary>
+  /// "quote"
+  /// 
+  /// `inline code block with "quote"`
+  /// 
+  /// ```cs
+  /// public void CodeBlock() {
+  ///   Console.WriteLine("Code block with quote");
+  /// }
+  /// ```
+  /// </summary>
+  public class Test {
+    // NYI
+  }
+}
